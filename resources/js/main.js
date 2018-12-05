b2455278701193220dbd25017e6df6531a3fa9d6
zuleta13 <unzuleta@gmail.com>
2018-12-05 19:00:41 +0100

    Alguna mejora añadida.

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,8 +4,10 @@
 				var val2 = parseInt($(".val2").val());
 				var val3 = parseInt($(".val3").val());
 				
+				$(".return").html("");
 				$(".return").append("<p>" + val3 + " " + (val3+val2) + " " + (2*val3+val2) + " " + (2*val3+ 2*val2) + " " + (2*val3+ 2*val2 + 2) +  "</p>");
 				$(".return").append("<p>" + val3/2 + " " + (val1 + val3/2) + " " + (val3+val1) + "</p>");
+				$(".return").append("<p>" + (2*val3+ 2*val2 + 2)*(val3+val1)*0.05/1000 + "</p>");
 			});
 			
 		});
